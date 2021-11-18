ecb3ac745040ab5695ad2ebc57bd7a88c837bb12
jay-choe <jay0764@naver.com>
2021-11-18 20:49:38 +0900

    캐시제거

--- order/build/resources/main/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product(id, name, price, stock)
-VALUES(1, "아이폰", 10000, 10);
-INSERT INTO product(id, name, price, stock)
-VALUES(2, "겔럭시", 20000, 10);
-INSERT INTO product(id, name, price, stock)
-VALUES(3, "LG", 30000, 10);
--- order/build/resources/main/schema.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists order_log;
-drop table if exists product;
-create table order_log (id integer not null auto_increment, order_stock integer, product_id integer, product_name varchar(255), product_price bigint, status varchar(255), primary key (id)) engine=InnoDB;
-create table product (id integer not null, name varchar(255), price bigint, stock integer, primary key (id)) engine=InnoDB;
