43c72cf2d130ccc215cc23bb86ed91d4c630d566
p-eye <standingtherain15@gmail.com>
2021-11-24 11:38:56 +0900

    문항을 위한 코드 삭제

--- order/src/main/java/async/example/service/OrderAsyncService.java
+++ order/src/main/java/async/example/service/OrderAsyncService.java
@@ -29,14 +29,11 @@ public class OrderAsyncService {
         Long totalPrice = product.getPrice() * requestStock;
         OrderLog orderLog = commonService.checkStockAndCreateOrder(product, requestStock);
 
-        // request payment, after payment request (구현해보세요)
-        log.info("========== 결제 요청 =============");
-        new Thread(() -> {
-            ResponseEntity<String> response = restTemplate.postForEntity(paymentUrl, totalPrice, String.class);
-            if (response.getBody().equals("결제 성공")) {
-                commonService.saveSuccessOrderAndUpdateStock2(product, requestStock, orderLog);
-            }
-            commonService.saveFailOrder2(orderLog, totalPrice);
-        }).start();
+        /*
+        request payment, after payment request (아래에 구현해보세요)
+        결제 요청 후 아래 메소드 사용 가능합니다.
+        commonService.saveSuccessOrderAndUpdateStock2
+        commonService.saveFailOrder2
+        */
     }
 }
